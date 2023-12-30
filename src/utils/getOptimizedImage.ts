f1f9020d44f24c727112d74d3783f64dd0bd22e1
Darlington Arinze <darlingtonarinze1995@gmail.com>
2023-12-30 19:41:23 +0100

    modify getOptimizedImage utils to handle no image url

--- src/utils/getOptimizedImage.ts
+++ src/utils/getOptimizedImage.ts
@@ -1,4 +1,7 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp'
+
 export const getOptimizedImage = (backgroundImageUrl: string) => {
+    if (!backgroundImageUrl) return noImage;
     const mediaString = 'media/';
     const mediaIndex = backgroundImageUrl.indexOf(mediaString) + mediaString.length;
     return `${backgroundImageUrl.slice(0, mediaIndex)}crop/600/400/${backgroundImageUrl.slice(mediaIndex)}`;
